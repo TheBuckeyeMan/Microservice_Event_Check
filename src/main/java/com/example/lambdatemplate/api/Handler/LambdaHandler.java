666d8ad0d8d1a97aa8be68ae137aba5250d352e8
Adam Maas <adamjm1220@gmail.com>
2024-10-24 11:22:25 -0400

    UpdatedMethod

--- src/main/java/com/example/lambdatemplate/api/Handler/LambdaHandler.java
+++ src/main/java/com/example/lambdatemplate/api/Handler/LambdaHandler.java
@@ -4,27 +4,24 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.example.lambdatemplate.config.LambdaConfig;
-import com.example.lambdatemplate.service.ExternalApiCall;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.example.lambdatemplate.service.CheckStatus;
+
 
 public class LambdaHandler implements RequestHandler<Object, Object> {
-    private ExternalApiCall externalApiCall;
+    private CheckStatus checkStatus;
 
     public LambdaHandler() {
         //check if we need thease two later
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LambdaConfig.class); //Set the application Configuration
         context.scan("com.example.lambdatemplate"); //Manually Tell Spring to Scan all contents of the project for Dependency Injection
-        externalApiCall = context.getBean(ExternalApiCall.class);
+        checkStatus = context.getBean(CheckStatus.class);
     }
 
     @Override
     public Object handleRequest(final Object input, final Context context) {
-        try{
-            externalApiCall.getFact();
-            //Add additional Service Methods Here
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return "API Successfully Triggered! File Saved to S3 Bucket!";
+        checkStatus.downloadLogFilesFromS3("logging-event-driven-bucket-1220-16492640", "Testing/Test.csv");
+        //Add additional Service Methods Here
+        return "Status Check Successfully Occured";
     }
 }
---
+++ src/main/java/com/example/lambdatemplate/service/EmailOnError.java
@@ -0,0 +1,13 @@
+package com.example.lambdatemplate.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailOnError {
+    
+
+
+
+
+
+}
